c1feded1bd1f88432c630c686a0f076a2e1cbc63
ka <ka.kaosf@gmail.com>
2015-09-26 02:48:57 +0900

    Initial commit

---
+++ automakekiss.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cat <<EOF > configure
+#!/bin/bash
+
+PREFIX=/usr/local
+if [[ \$1 == --prefix=* ]]; then
+  PREFIX=\`echo \$1 | sed 's/--prefix=//'\`
+fi
+sed "s|PREFIX=|PREFIX=\${PREFIX}|" Makefile.in > Makefile
+EOF
+chmod +x configure
+
+cat <<EOF > Makefile.in
+PREFIX=
+
+all: a.out
+
+a.out: foo.c
+	gcc foo.c
+
+install:
+	mkdir -p $(PREFIX)/bin
+	cp a.out $(PREFIX)/bin
+
+uninstall:
+	rm $(PREFIX)/bin/a.out
+
+clean:
+	rm a.out
